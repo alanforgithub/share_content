4726340deba98e56c9991989d65563a4d025843e
alanforgithub <guangbingwang@126.com>
2015-04-03 14:40:43 +0800

    add files

--- HttpDemo/src/com/http/httpdemo/MainActivity.java
+++ HttpDemo/src/com/http/httpdemo/MainActivity.java
@@ -10,6 +10,8 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,16 +21,20 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.DefaultClientConnection;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -43,7 +49,13 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		mHttpClient = new DefaultHttpClient();
+		HttpParams params = new BasicHttpParams();
+		HttpProtocolParams.setContentCharset(params, HTTP.UTF_8); // 设置编码格式
+		// /* 连接超时 */
+		// HttpConnectionParams.setConnectionTimeout(params, 10000);
+		// /* 请求超时 */
+		// HttpConnectionParams.setSoTimeout(params, 10000);
+		mHttpClient = new DefaultHttpClient(params);
 
 		findViewById(R.id.getbutton).setOnClickListener(new OnClickListener() {
 
@@ -94,20 +106,34 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void run() {
-				HttpPost post = new HttpPost(uriPost);
+				HttpPost post = new HttpPost(
+						"http://www.nonghule.com/app/login.php?type=onlinedo");
 				try {
 					List<BasicNameValuePair> list = new ArrayList<BasicNameValuePair>();
-					list.add(new BasicNameValuePair("keyfrom", "testHttpGet"));
-					list.add(new BasicNameValuePair("key", "850021564"));
-					list.add(new BasicNameValuePair("type", "data"));
-					list.add(new BasicNameValuePair("doctype", "json"));
-					list.add(new BasicNameValuePair("version", "1.1"));
-					list.add(new BasicNameValuePair("q", "bad"));
-					post.setEntity(new UrlEncodedFormEntity(list));
+					list.add(new BasicNameValuePair("useracc", "alan_nonghule"));
+					list.add(new BasicNameValuePair("userpw", md5("alan123456")));
+					post.setEntity(new UrlEncodedFormEntity(list,HTTP.UTF_8));
 
 					HttpResponse response = mHttpClient.execute(post);
 					String value = EntityUtils.toString(response.getEntity());
+
+					char[] chars = value.toCharArray();
+					for (int i = 0; i < chars.length; i++) {
+						Log.i("android", i + " = " + chars[i]);
+					}
+
 					Log.i("android", "httpClient post方式 = " + value);
+
+					Log.i("android", "httpClient post方式 =" + value + ",size = "
+							+ value.length());
+
+					try {
+						JSONObject jsonObj = new JSONObject(value);
+						Log.i("android", "数据解析成功");
+					} catch (JSONException e) {
+						e.printStackTrace();
+						Log.i("android", "数据解析错误");
+					}
 
 				} catch (UnsupportedEncodingException e) {
 					// TODO Auto-generated catch block
@@ -125,6 +151,26 @@ public class MainActivity extends Activity {
 
 	}
 
+	private String md5(String in) {
+		byte[] hash;
+		try {
+			hash = MessageDigest.getInstance("MD5")
+					.digest(in.getBytes("UTF-8"));
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("Huh, MD5 should be supported?", e);
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("Huh, UTF-8 should be supported?", e);
+		}
+
+		StringBuilder hex = new StringBuilder(hash.length * 2);
+		for (byte b : hash) {
+			if ((b & 0xFF) < 0x10)
+				hex.append("0");
+			hex.append(Integer.toHexString(b & 0xFF));
+		}
+		return hex.toString();
+	}
+
 	/**
 	 * HttpGet
 	 */
@@ -133,12 +179,24 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void run() {
-				HttpGet get = new HttpGet(uriGet);
+				HttpGet get = new HttpGet(
+						"http://www.nonghule.com/app/login1.php?type=onlinedo&useracc=admin&userpw=eeea66479e7b5401f23befbb494bfcde");
 				try {
 					HttpResponse response = mHttpClient.execute(get);
 					String value = EntityUtils.toString(response.getEntity());
 
-					Log.i("android", "httpClient get方式 = " + value);
+					//String value1 = new String( value.toString().getBytes( "utf-8" ), "gbk");
+					
+					Log.i("android", "httpClient get方式 =" + value.trim() + ",size = "
+							+ value.length());
+
+					try {
+						JSONObject jsonObj = new JSONObject(value);
+						Log.i("android", "数据解析成功");
+					} catch (JSONException e) {
+						e.printStackTrace();
+						Log.i("android", "数据解析错误");
+					}
 
 				} catch (ClientProtocolException e) {
 					e.printStackTrace();
@@ -149,7 +207,6 @@ public class MainActivity extends Activity {
 		}).start();
 	}
 
-	
 	private void httpGet() {
 		new Thread(new Runnable() {
 
